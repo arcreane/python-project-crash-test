be374af6e9f9164d275aec542d1a23260ae8d210
maaxaa16 <maxime.roetynck@gmail.com>
2025-12-06 15:53:55 +0100

    ajout des conditons pour atterisage dans la Classe

--- Plane.py
+++ Plane.py
@@ -1,6 +1,6 @@
 import math
 
-class Plane:
+class Airplane:
     counter = 1
 
     def __init__(self, x, y, angle_deg, image):
@@ -12,14 +12,37 @@ class Plane:
         self.w = image.width()
         self.h = image.height()
 
-        self.name = f"Vol n°{Plane.counter}"
-        Plane.counter += 1
+        self.name = f"Vol n°{Airplane.counter}"
+        Airplane.counter += 1
 
         self.must_land = False
         self.emergency = False
         self.destination = "Transit"
 
         self.holding = False
+
+        self.landing = False
+        self.waypoints = []
+        self.current_wp = 0
+
+    def reached_waypoint(self):
+        if not self.waypoints:
+            return False
+
+        tx, ty = self.waypoints[self.current_wp]
+        cx = self.x + self.w/2
+        cy = self.y + self.h/2
+
+        return math.dist((cx, cy), (tx, ty)) < 15
+
+    def update_landing_angle(self):
+        tx, ty = self.waypoints[self.current_wp]
+        cx = self.x + self.w/2
+        cy = self.y + self.h/2
+
+        self.angle = math.degrees(
+            math.atan2(tx - cx, cy - ty)
+        )
 
     def update_position(self, max_l, max_h):
         rad = math.radians(self.angle)
