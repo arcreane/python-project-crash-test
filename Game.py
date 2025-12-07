69b399e65ed5268994ea19209bcf04d6a98d9b33
maaxaa16 <maxime.roetynck@gmail.com>
2025-12-07 22:19:03 +0100

    Ajout du fenetre de demarrage, modif sur Game(ajout avions clic)

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ class GameEngine:
     Gère :
     - score
     - temps sans crash
+    - bonus atterrissage / malus avion perdu
     - +1 point toutes les 10 s
     """
 
@@ -14,57 +15,71 @@ class GameEngine:
         self.score = 0
         self.managed_planes = 0
         self.survival_time = 0
+        self.selected_planes = set()
 
         # Timer 1s → temps sans crash
         self.survival_timer = QTimer(sim)
-        self.survival_timer.timeout.connect(self._on_survival_tick)
+        self.survival_timer.timeout.connect(self.survival_tick)
         self.survival_timer.start(1000)
 
         # Timer 10s → +1 point
         self.score_timer = QTimer(sim)
-        self.score_timer.timeout.connect(self._on_score_tick)
+        self.score_timer.timeout.connect(self.score_tick)
         self.score_timer.start(10000)
 
-        # Initialisation label
-        if hasattr(self.sim.ui, "labelScore"):
-            self.sim.ui.labelScore.setText("Score : 0")
-        if hasattr(self.sim.ui, "labelTimer"):
-            self.update_timer_label()
 
-    # ---------- Ticks ----------
+        self.sim.ui.labelScore.setText("Score : 0")
 
-    def _on_survival_tick(self):
+
+    def survival_tick(self):
         if not self.sim.game_over:
             self.survival_time += 1
-            self.update_timer_label()
+            self.timer_label()
+            self.sim.update_info_label()  # met à jour label avion si sélectionné
 
-    def _on_score_tick(self):
+    def score_tick(self):
         if not self.sim.game_over:
             self.score += 1
             self.update_score_label()
 
-    def update_timer_label(self):
+    def timer_label(self):
+        """Affiche le timer"""
         minutes = self.survival_time // 60
         seconds = self.survival_time % 60
         text = f"⏱ {minutes:02d}:{seconds:02d}"
 
-        if hasattr(self.sim.ui, "labelTimer"):
-            self.sim.ui.labelTimer.setText(text)
+        self.sim.ui.labelTimer.setText(text)
 
-    # ---------- API ----------
 
-    def add_managed_plane(self):
+    def register_click(self, plane):
+        """Appelé quand on clique sur un avion pour ajouter dans les avions gérer."""
+        self.selected_planes.add(plane)
+
+    def add_landing_score(self, plane):
+        """Bonus quand un avion atterrit correctement."""
+        self.score += 5
+        self.update_score_label()
+
+    def add_missed_penalty(self, plane):
+        """Malus si avion quitte l'écran sans avoir été cliqué."""
+        if plane not in self.selected_planes and not self.sim.game_over:
+            self.score -= 3
+            self.update_score_label()
+
+    def add_managed_plane(self, plane):
+        """Compteur d’avions gérés correctement."""
         self.managed_planes += 1
         self.update_score_label()
 
     def update_score_label(self):
-        if hasattr(self.sim.ui, "labelScore"):
-            self.sim.ui.labelScore.setText(
-                f"Score : {self.score}\n\n"
-                f"Avions gérés : {self.managed_planes}"
-            )
+        self.sim.ui.labelScore.setText(
+            f"Score : {self.score}\n"
+            f"\n"
+            f"\n"
+            f"Avions gérés : {self.managed_planes}\n"
+        )
 
     def stop_all(self):
-        """Appelé sur collision pour couper les timers de jeu."""
+        """Appelé sur collision pour couper les timers."""
         self.survival_timer.stop()
         self.score_timer.stop()
---
+++ main.py
@@ -0,0 +1,98 @@
+import sys
+import time
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtUiTools import loadUiType
+from PySide6.QtCore import Slot,Qt,QUrl
+from PySide6.QtGui import QPainter,QPixmap
+from PySide6.QtMultimedia import QSoundEffect
+
+
+
+class Home(QMainWindow):
+    def __init__(self, sim_window):
+        super().__init__()
+        Ui_Home, _ = loadUiType("Home.ui")
+        self.ui = Ui_Home()
+        self.ui.setupUi(self)
+        self.home = QPixmap("image/atc.jpg")
+
+        self.clic = QSoundEffect()
+        self.clic.setSource(QUrl.fromLocalFile("audio/airplanecabin.wav"))
+        self.clic.setLoopCount(1)
+        self.clic.setVolume(0.9)
+
+        self.back = QSoundEffect()
+        self.back.setSource(QUrl.fromLocalFile("audio/back.wav"))
+        self.back.setLoopCount(1)
+        self.back.setVolume(0.5)
+
+        self.music_muted = False
+
+        self.sim_window = sim_window
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        if not self.music_muted:
+            self.back.play()
+
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        W, H = self.width(), self.height()
+        bg = self.home.scaled(W, H, Qt.KeepAspectRatioByExpanding)
+        painter.drawPixmap(0, 0, bg)
+
+
+    @Slot()
+    def Musique(self):
+        if not self.music_muted:
+            self.back.stop()
+            self.music_muted = True
+        else:
+            self.back.play()
+            self.music_muted = False
+
+    @Slot()
+    def Level1(self):
+        level = 7000
+        self.clic.play()
+        time.sleep(2)
+        self.sim_window.spawn_level = level
+        self.sim_window.spawn_timer.start(level)
+
+        self.sim_window.showMaximized()
+        self.hide()
+
+    @Slot()
+    def Level2(self):
+        level = 5000
+        self.clic.play()
+        time.sleep(2)
+        self.sim_window.spawn_level = level
+        self.sim_window.spawn_timer.start(level)
+
+        self.sim_window.showMaximized()
+        self.hide()
+
+    @Slot()
+    def Level3(self):
+        level = 3000
+        self.clic.play()
+        time.sleep(2)
+        self.sim_window.spawn_level = level
+        self.sim_window.spawn_timer.start(level)
+
+        self.sim_window.showMaximized()
+        self.hide()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    from app import Simulation
+    simulation_window = Simulation()
+    simulation_window.hide()
+
+    HOME = Home(simulation_window)
+
+    HOME.showMaximized()
+    sys.exit(app.exec())
